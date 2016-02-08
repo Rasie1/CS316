e88c8deae6dc8f27b3f1c804c3ffb2a58aefb18e
Rasie1 <rasielll@gmail.com>
2016-02-08 12:25:11 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	bool shouldContinue = true;
+	while (shouldContinue)
+	{
+		std::cout << "> ";
+		std::string input;
+		std::cin >> input;
+
+		
+	}
+	return 0;
+}
