b62b1dbacad66b20d1b8d950d7eb15b41c2f8e12
Vsevolod Kvachev <rasielll@gmail.com>
2016-02-29 12:25:25 +0300

    Some method placeholders and refactoring

--- Task2/Classes/MyMatrix/Matrix.h
+++ Task2/Classes/MyMatrix/Matrix.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <stdexcept>
 
 template<typename T>
@@ -8,19 +7,34 @@ class Matrix
 public:
     Matrix(size_t rows, size_t cols);
 
-    virtual ~Matrix() {}
+    Matrix(const Matrix&) = default;
+    Matrix(Matrix&&) = default;
+    Matrix& operator=(const Matrix&) & = default;
+    Matrix& operator=(Matrix&&) & = default;
+    virtual ~Matrix() { }
 
     T  operator()(size_t x, size_t y) const throw(std::out_of_range());
     T& operator()(size_t x, size_t y)       throw(std::out_of_range());
+
+
 
     size_t rows() const;
     size_t cols() const;
     std::pair<size_t, size_t> size() const;
 
-    void resize(size_t rows, size_t cols);
+    void resize(size_t rows, size_t cols, const T& value = T());
+
+    template<typename ForwardIt>
+    void insertCol(ForwardIt begin, ForwardIt end);
+    template<typename ForwardIt>
+    void insertRow(ForwardIt begin, ForwardIt end);
+
+    void deleteRow(size_t n);
+    void deleteCol(size_t n);
 
 private:
     size_t nRows, nCols;
+
     T *data;
 };
 
--- Task2/Classes/MyMatrix/Matrix_Impl.h
+++ Task2/Classes/MyMatrix/Matrix_Impl.h
@@ -27,6 +27,44 @@ size_t Matrix<T>::cols() const
 }
 
 template<typename T>
+void Matrix<T>::deleteRow(size_t n)
+{
+
+}
+
+template<typename T>
+void Matrix<T>::deleteCol(size_t n)
+{
+
+}
+
+template<typename T>
+template<typename ForwardIt>
+void Matrix<T>::insertCol(ForwardIt begin, ForwardIt end)
+{
+
+}
+
+template<typename T>
+template<typename ForwardIt>
+void Matrix<T>::insertRow(ForwardIt begin, ForwardIt end)
+{
+
+}
+
+static inline size_t indexFromCoordinates(size_t x,
+                                          size_t y,
+                                          size_t rows,
+                                          size_t cols) throw(std::out_of_range())
+{
+
+    if (x < rows && y < cols)
+        return y * cols + x;
+    else
+        throw std::out_of_range("You're out of matrix size bounds");
+}
+
+template<typename T>
 T Matrix<T>::operator()(size_t x, size_t y) const throw(std::out_of_range())
 {
     return operator()(x, y);
@@ -35,17 +73,25 @@ T Matrix<T>::operator()(size_t x, size_t y) const throw(std::out_of_range())
 template<typename T>
 T& Matrix<T>::operator()(size_t x, size_t y) throw(std::out_of_range())
 {
-    if (x < rows() && y < cols())
-        return data[y * cols() + x];
-    else
-        throw std::out_of_range("You're out of matrix size bounds");
+    return data[indexFromCoordinates(x,y, rows(), cols())];
 }
 
 template<typename T>
-void Matrix<T>::resize(size_t rows, size_t cols)
+void Matrix<T>::resize(size_t rows, size_t cols, const T& value)
 {
+    auto newData = new T[rows * cols];
+
+    // fill with default
+    for (int y = 0; y < nRows; ++y)
+    for (int x = 0; x < nCols; ++x)
+    {
+        operator () (x, y) = value;
+    }
+
+    delete[] data;
+
+    this->data = newData;
     this->nRows = rows;
     this->nCols = cols;
-    data = new T[rows * cols];
 }
 
