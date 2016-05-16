908ce2b33a54b495dd74d330513803dbe6637acb
Vsevolod Kvachev <rasielll@gmail.com>
2016-05-16 12:43:46 +0300

    remoed CowPtr impl

--- Task2/Classes/MyMatrix/Matrix.h
+++ Task2/Classes/MyMatrix/Matrix.h
@@ -5,61 +5,6 @@
 #include <memory>
 #include "SharedPointer.h"
 
-template <class T>
-class CowPtr
-{
-    public:
-        typedef SharedPointer<T> RefPtr;
-
-    private:
-        RefPtr m_sp;
-
-        void detach()
-        {
-            T* tmp = m_sp.get();
-            if( !( tmp == 0 || m_sp.unique() ) ) {
-                m_sp = RefPtr( new T( *tmp ) );
-            }
-        }
-
-    public:
-        CowPtr(T* t)
-            :   m_sp(t)
-        {}
-        CowPtr(const RefPtr& refptr)
-            :   m_sp(refptr)
-        {}
-        const T& operator*() const
-        {
-            return *m_sp;
-        }
-        T& operator*()
-        {
-            detach();
-            return *m_sp;
-        }
-        const T* operator->() const
-        {
-            return m_sp.operator->();
-        }
-        T* operator->()
-        {
-            detach();
-            return m_sp.operator->();
-        }
-};
-
-
-
-
-
-
-
-
-
-
-
-
 template<typename T, class A = std::allocator<T>>
 class Matrix
 {
