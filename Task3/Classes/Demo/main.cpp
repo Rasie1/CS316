f14d5c37229ab5646282b986b47d658780080590
Vsevolod Kvachev <rasielll@gmail.com>
2016-05-27 00:19:58 +0300

    task4

--- Task3/Classes/Demo/main.cpp
+++ Task3/Classes/Demo/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "World.h"
 
 using namespace std;
 
 int main()
 {
-    auto width  = 64;
-    auto height = 64;
+    auto width  = 20;
+    auto height = 20;
     auto population = 0.3;
 
     auto world = World(width, height, population);
 
     auto iterations = 32;
+    auto delay = 500;
 
     cout << "Starting simulation with" << endl
          << "Width: " << width << endl
          << "Height: " << height << endl
          << "Population percentage: " << population << endl;
 
-    for (int i = 0; i < 32; ++i)
+    for (int i = 0; i < iterations; ++i)
     {
         world.step();
         cout << world << endl;
+        this_thread::sleep_for(std::chrono::milliseconds(delay));
     }
 
     cout << "Simulation finished" << endl;
---
+++ Task4/Classes/Demo/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+    const int n = 6;
+    cout << "Magic ret of " << n << endl;
+
+    return 0;
+}
